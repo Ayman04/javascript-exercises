db0cc406587d69ec98fb178d47e053ba881f4002
Ayman04 <95933706+Ayman04@users.noreply.github.com>
2022-12-18 21:23:26 +0100

    "repeatstring done"

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    for(let i = string.length - 1; i  > -1; i--){
+        newString = newString + string[i];
+    }
+    return newString
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
