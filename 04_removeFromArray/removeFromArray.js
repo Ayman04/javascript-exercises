9f408a19373e3da81ea9f6e056793cb95c491763
Ayman04 <95933706+Ayman04@users.noreply.github.com>
2022-12-20 17:47:10 +0100

    "done too"

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {
+    array = array.filter((value)=>{
+        return value !== item
+    });
+    return array
 };
-
+removeFromArray([1,2,3,4,5],3)
 // Do not edit below this line
 module.exports = removeFromArray;
