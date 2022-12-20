0f8a4690538af706504a1a379a1b19d9df2a426a
Ayman04 <95933706+Ayman04@users.noreply.github.com>
2022-12-20 18:10:34 +0100

    "again too easy"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion = (temp - 32) / 1.8;
+  let solution = (Math.round(conversion * 10) / 10);
+  return solution
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = temp * 1.8 + 32;
+  let solution = (Math.round(conversion * 10) / 10)
+  return solution
 };
 
+
+console.log(convertToCelsius(68), convertToFahrenheit(20));
 // Do not edit below this line
-module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
-};
+// module.exports = {
+//   convertToCelsius,
+//   convertToFahrenheit
+// };
