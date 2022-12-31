8c98d6fb778d24585460ee6710baf19e15a50a77
Ayman04 <95933706+Ayman04@users.noreply.github.com>
2022-12-31 15:20:45 +0100

    verkackt

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let array = [1,1,2]
+    for(let i = num - 3; i <  num;i++){
+        
+    }
 };
 
 // Do not edit below this line
